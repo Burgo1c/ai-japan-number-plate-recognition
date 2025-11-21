a1b37b528abd2d3f129c599bae46a56afabf586d
Burgo1c <72175382+Burgo1c@users.noreply.github.com>
2025-11-21 14:29:17 +0900

    Delete export.py

--- export.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load your PyTorch model
-model = YOLO('ai-model/best.pt') 
-
-# Export to Edge TPU format
-# Specify an image size (imgsz) that matches your training, e.g., 640
-model.export(format='edgetpu', imgsz=640)
